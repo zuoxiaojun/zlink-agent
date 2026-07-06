1ac654a9c2e32b1f99405fa7678707d4a4023eab
zuoxiaojun <zuoxiaojun@users.noreply.gitee.com>
2026-07-06 15:31:09 +0800

    revert: 移除 Electron/PyInstaller 客户端打包配置，回归纯 BS 架构

--- electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  platform: process.platform,
-  versions: {
-    node: process.versions.node,
-    electron: process.versions.electron,
-  },
-});
